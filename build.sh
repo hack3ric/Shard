7e40fb172ef3d1de7b79e6fc15b0d665194aa10f
Eric Long <i@hack3r.moe>
2026-06-29 14:21:24 +0800

    cmake release build

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ -d "build" ]; then
-	rm -r build
+  rm -r build
 fi
 mkdir build
 cd build
-cmake ..
+cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
 cp ../patch/libmlx5.so libmlx5.so
 echo 'Build completed'
